5148654fc2c69420aa0d74b9fe8951d51be9e731
Jay <36286598+Theqwertypusher@users.noreply.github.com>
2020-09-12 01:20:15 -0400

    (added) api router and controller

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const path = require('path');
+
+const apiRouter = require('./routes/api.js');
+
 const app = express();
 const PORT = 3000;
 
-
+// serve static files
 app.get('/', (req, res) => {
-    express.static(path.resolve(__dirname, "../index.html"))
+    express.static(path.resolve(__dirname, "../index.html"));
 })
 
-app.listen(PORT, () => console.log(`listenning on localhost ${PORT}`))
+// API route handler
+app.use('/api', apiRouter);
+
+// USER route handler
+
+// catch-all error handler
+
+// global error handler
+
+app.listen(PORT, () => console.log(`listenning on localhost ${PORT}`));
