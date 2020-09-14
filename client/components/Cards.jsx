69c65d11fe9ee5b37cbeb265deb727d6b574c63f
Rocio-Infante <shadesofroseblog@gmail.com>
2020-09-13 19:26:46 -0700

    Updated card.jsx file"

--- client/components/Cards.jsx
+++ client/components/Cards.jsx
@@ -6,6 +6,7 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+//styling 
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
